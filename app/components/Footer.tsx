b6ce9e42d4fa238db96928acc7819d7562ed5d44
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-10-15 19:18:50 -0400

    Changing background color

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import Colors from '../utils/Colors';
 const Footer: React.FC<any> = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Herramienta de uso veterianrio.</Text>
+      <Text style={styles.text}>Herramienta de uso veterinario.</Text>
       <Text style={styles.text}>
         No nos hacemos responsables del mal uso de esta aplicacion.
       </Text>
@@ -16,7 +16,7 @@ const Footer: React.FC<any> = () => {
 export default Footer;
 
 const styles = StyleSheet.create({
-  container: { padding: 5 },
+  container: { padding: 5, backgroundColor: Colors.primary },
   text: {
     fontSize: 12,
     textAlign: 'center',
