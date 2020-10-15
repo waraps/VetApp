f3f8ed378d35f28e50a5f3db95a25a569ecc3bb0
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-10-15 19:18:24 -0400

    Adding colors pallete

--- app/utils/Colors.js
+++ app/utils/Colors.js
@@ -1,12 +1,10 @@
 const Colors = {
-  primary: '#1292B4',
-  white: '#FFF',
+  primary: '#084887',
+  secondary: '#1fa6d4',
   lighter: '#F3F3F3',
   light: '#DAE1E7',
-  dark: '#444',
-  black: '#000',
-  blue: '#1B1464',
-  green: '#006266',
+  dark: '#555',
+  darkness: '#444',
 };
 
 export default Colors;
