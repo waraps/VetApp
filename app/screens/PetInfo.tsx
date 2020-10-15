a9ed334ef530895ab98010701e5883bb321681f2
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-10-14 20:22:32 -0400

    Adding selection pet feature

--- app/screens/PetInfo.tsx
+++ app/screens/PetInfo.tsx
@@ -1,13 +1,39 @@
+/* eslint-disable global-require */
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+// Types
 import { PetInfoScreenProps } from '../types/props/PetInfoScreenPropsTypes';
 
-const PetInfo: React.FC<PetInfoScreenProps> = (props) => {
+// Components
+import PetCard from '../components/PetCard';
+
+const PetInfo: React.FC<PetInfoScreenProps> = ({ navigation }) => {
+  const nextScreen = () => {
+    navigation.push('MedicationList');
+  };
   return (
-    <View>
-      <Text>Pet</Text>
+    <View style={styles.container}>
+      <PetCard
+        title="Canino"
+        navigateTo={nextScreen}
+        imagePath={require('../assets/img/dog.png')}
+      />
+      <PetCard
+        title="Felino"
+        navigateTo={nextScreen}
+        imagePath={require('../assets/img/cat.png')}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+});
 
 export default PetInfo;
