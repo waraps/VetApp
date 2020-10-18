4d41ef9ff88467f47ac4603b15ed44cdcd82e299
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-10-18 15:32:59 -0400

    Adding medicines data mapping feature

---
+++ app/utils/helpers.ts
@@ -0,0 +1,34 @@
+import { MedicineJSON, Medicine } from '../types/medicines/medicineType';
+
+const mapMedicines = (
+  petType: string,
+  medicineList: Array<MedicineJSON>,
+): Array<Medicine> => {
+  let mapMedicineList: Array<Medicine> = [];
+  switch (petType) {
+    case 'canino':
+      mapMedicineList = medicineList.map((medicine) => {
+        return {
+          name: medicine.name,
+          pet: medicine.dog,
+          concentration: medicine.concentration,
+        };
+      });
+      break;
+    case 'felino':
+      mapMedicineList = medicineList.map((medicine) => {
+        return {
+          name: medicine.name,
+          pet: medicine.cat,
+          concentration: medicine.concentration,
+        };
+      });
+      break;
+    default:
+      mapMedicineList = [];
+      break;
+  }
+  return mapMedicineList;
+};
+
+export default mapMedicines;
