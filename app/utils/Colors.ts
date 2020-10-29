553c623ca3d75cf9bc592a2e549bab3e17cad783
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-10-29 19:57:28 -0400

    Adding primaryTranslucent color

--- app/utils/Colors.ts
+++ app/utils/Colors.ts
@@ -1,10 +1,13 @@
 const Colors = {
   primary: '#084887',
+  primaryLight: '#094f9e',
+  primaryTranslucent: 'rgba(8, 72, 135, 0.22)',
   secondary: '#1fa6d4',
   lighter: '#F3F3F3',
   light: '#DAE1E7',
   dark: '#555',
   darkness: '#444',
+  gray: '#ccc',
 };
 
 export default Colors;
