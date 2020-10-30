8c8c809a48ec62c866dc58e1956d8ce69bb14557
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-10-29 20:49:44 -0400

    Changing translucent color intensity

--- app/utils/Colors.ts
+++ app/utils/Colors.ts
@@ -1,7 +1,7 @@
 const Colors = {
   primary: '#084887',
   primaryLight: '#094f9e',
-  primaryTranslucent: 'rgba(8, 72, 135, 0.22)',
+  primaryTranslucent: 'rgba(8, 72, 135, 0.05)',
   secondary: '#1fa6d4',
   lighter: '#F3F3F3',
   light: '#DAE1E7',
