f70bba57dfc8e8066465199fec77b850155c6e80
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-10-30 21:19:22 -0400

    Showing selected medicaments

--- app/screens/Results.tsx
+++ app/screens/Results.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Results: React.FC<any> = ({ route }) => {
   const medicationList = route.params.medicineList;
   return (
     <View>
-      <Text>{JSON.stringify(medicationList)}</Text>
+      {medicationList.map((medicine) => {
+        return <Text style={styles.text}>{medicine.name}</Text>;
+      })}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    padding: 5,
+    fontSize: 15,
+  },
+});
 
 export default Results;
